75a9aed7fb853c40a67d53af58857f961419a72a
Tomislav Dukez <tomdu3@ymail.com>
2026-06-10 11:57:04 +0100

    refactor image2sketch to be used as script

---
+++ utils/image2sketch.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import cv2
+import os
+import sys
+
+
+def image2sketch(image_path):
+    image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    invert = 255 - gray
+    blur = cv2.GaussianBlur(invert, (21, 21), 0)
+    sketch = cv2.divide(gray, 255 - blur, scale=256)
+    base_name = os.path.splitext(os.path.basename(image_path))[0]
+    directory = os.path.dirname(image_path)
+    output_path = os.path.join(directory, f"{base_name}_sketch.png")
+    cv2.imwrite(output_path, sketch)
+    return output_path
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python image2sketch.py <image_path>")
+        sys.exit(1)
+    image_path = sys.argv[1]
+    output_path = image2sketch(image_path)
+    print(f"Image converted to sketch: {output_path}")
+
